5ba5b22dcc5df79aeedc905abf408f5de2868a3a
ChristofferFredsgaard <chri97c1@edu.sde.dk>
2022-11-09 12:49:12 +0100

    Configured Firebase files

--- components/screens/ProfileScreen/index.js
+++ components/screens/ProfileScreen/index.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import { Text, View, Image } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import firebase from '../database/connection';
 
-import styles from './styles'
-import Header from '../../header/header'
 
-const image = { uri: "https://freepngimg.com/thumb/wave/110541-white-wave-free-hq-image.png" };
-
-const ProfileScreen = () => {
-  return (
-    <View>
-        <View style={styles.ContentContainer}>
-          <Header />
-
-          <View style={styles.logo}>
-            <Image source={image} style={styles.image}></Image>
-          </View>
-          
-          <View style={styles.titles}>
-              <Text style={styles.title}>PROFILE</Text>
-              <Text style={styles.subtitle}>Here at BetterBetter you will be able to determine your bets from specific data, 
-                                            that you can find for every match you search up. It has never 
-                                            been easier to find the average amount of corners, 
-                                            goals scored or goals conceded per match. Create your bets, with the help of our data.</Text>
-          </View>
-        </View>
-    </View>
-  );
+export default class ProfileScreen extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      uid: ''
+    }
+  }
+  signOut = () => {
+    firebase.auth().signOut().then(() => {
+      this.props.navigation.navigate('Login')
+    })
+    .catch(error => this.setState({ errorMessage: error.message }))
+  }  
+  render() {
+    this.state = { 
+      displayName: firebase.auth().currentUser.displayName,
+      uid: firebase.auth().currentUser.uid
+    }    
+    return (
+      <View style={styles.container}>
+        <Text style = {styles.textStyle}>
+          Hello, {this.state.displayName}
+        </Text>
+        <Button
+          color="#3740FE"
+          title="Logout"
+          onPress={() => this.signOut()}
+        />
+      </View>
+    );
+  }
 };
-
-export default ProfileScreen;
